b42757a2119d78838f6220e27d005b4010bea698
MaxDevcic <ma.devcic@duocuc.cl>
2025-06-09 11:01:30 -0400

    first commit

---
+++ src/main/java/com/pruebas1/unitarias1/Unitarias1Application.java
@@ -0,0 +1,13 @@
+package com.pruebas1.unitarias1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unitarias1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unitarias1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebas1/unitarias1/Unitarias1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebas1.unitarias1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unitarias1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
