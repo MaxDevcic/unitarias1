155e2f7aad46e5322b3592ae776fe5405685de79
MaxDevcic <ma.devcic@duocuc.cl>
2025-06-11 13:40:49 -0400

    test obtener mascotapor id

--- src/test/java/com/pruebas1/unitarias1/service/MascotaServiceTest.java
+++ src/test/java/com/pruebas1/unitarias1/service/MascotaServiceTest.java
@@ -8,9 +8,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-/*import java.util.Optional;
+/*import java.util.Optional;*/
 import java.util.Arrays;
-import java.util.List;*/
+import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,4 +44,35 @@ class MascotaServiceTest {
         assertThat(resultado.getEdad()).isEqualTo(5);
         verify(mascotaRepository).save(mascota);
     }
+
+     @Test
+    void testListarMascotas() {
+        Mascota m1 = new Mascota(1L, "Rex", "Perro", 5);
+        Mascota m2 = new Mascota(2L, "Michi", "Gato", 2);
+        when(mascotaRepository.findAll()).thenReturn(Arrays.asList(m1, m2));
+
+        List<Mascota> resultado = mascotaService.listarMascotas();
+        assertThat(resultado).hasSize(2).contains(m1, m2);
+        verify(mascotaRepository).findAll();
+    }
+
+    @Test
+    void testObtenerMascotaPorId() {
+        Mascota mascotaencontrada = new Mascota(1L, "Rex", "Perro", 5);
+        when(mascotaRepository.findById(1L)).thenReturn(Optional.of(mascotaencontrada));
+
+        Optional<Mascota> resultado = mascotaService.obtenerMascotaPorId(1L);
+        //assertThat(resultado.get().getId()).isEqualTo(1L);
+        assertThat(resultado).isPresent().contains(mascotaencontrada);
+        verify(mascotaRepository).findById(1L);
+    }
+
+    
+     @Test
+     void testActualizarMascota() {
+        Mascota mascotaAntigua = new Mascota(1L, "Rex", "Perro", 5);
+        Mascota mascotaNueva = new Mascota(1L, "Rex", "Perro", 6);
+        when(mascotaRepository);
+     }
+     
 }
